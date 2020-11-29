f821d7940b912b94cfdbbb23a2a6164f2f115cff
VirangParekh <44228173+VirangParekh@users.noreply.github.com>
2020-11-29 17:02:41 +0530

    Formatting Problem 2, Solved.

---
+++ pindicator/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from pindicator.users.models import User
+from pindicator.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
